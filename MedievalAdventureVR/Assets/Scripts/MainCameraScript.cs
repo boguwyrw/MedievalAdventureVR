d69451e4026d2949cdd31145e463cf10e55a523f
boguwyrw <b.wyrwal@wp.pl>
2020-08-30 13:54:47 +0200

    Camera controller - in progress

---
+++ MedievalAdventureVR/Assets/Scripts/MainCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class MainCameraScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        var inputDevices = new List<InputDevice>();
+        
+
+        foreach (var device in inputDevices)
+        {
+            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+        }
+        /*
+        //XRSettings.enabled = false;
+        Camera.main.GetComponent<Transform>().localPosition = InputTracking.GetNodeStates(HumanPose);
+        //Camera.main.GetComponent<Transform>().localRotation = InputTracking.GetLocalRotation(XRNode.CenterEye);
+        */
+    }
+}
